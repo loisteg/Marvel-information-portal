78c26edca3a0b41c2ae13f62fbc95629db4b9056
Loisteg <loisteg@outlook.com>
2022-02-20 21:34:50 +0200

    First commit(added random and cards on first screen)

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,81 @@
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+
+
+class CharList extends Component {
+    
+    state = {
+        charList: [],
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.marvelService.getAllCharacters()
+        .then(this.onCharListLoaded)
+        .catch(this.onError)
+    }
+
+    onCharListLoaded = (charList) => {
+        this.setState({
+            charList,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        const items = arr.map(item => {
+            let imgStyle;
+            imgStyle = item.thumbnail.includes('image_not_available') ? imgStyle = {'objectFit' : 'unset'} : {'objectFit' : 'cover'};
+            return (
+                <li className="char__item">
+                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                    <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
+
+
+
+    render() {
+        const {charList, loading, error} = this.state;
+
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default CharList;
